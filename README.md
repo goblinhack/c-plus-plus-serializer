881ec852a21ffe1df6e7a98bedd1d2cd03c4f5db
Neil McGill <goblinhack@gmail.com>
2019-09-06 19:46:26 -0400

    support simple containers list vector and list

--- README.md
+++ README.md
@@ -1,9 +1,57 @@
 Simple c++ serializer
 =====================
 
-TODO
+A very minimal C++14 header only serializer. No sanity checking is performed
+on the data when it is read back in, so make sure your serializer and 
+deserializers match perfectly. Example usege:
+
+saving:
 
 <pre>
-TODO
+#include "c_plus_plus_serializer.h"
+
+static void save (std::ofstream f)
+{
+    int a = 5, b = -1, c = 123456;
+    std::string d("hello");
+    std::initializer_list<std::string> data = {"there", "you"};
+    std::vector<std::string> e(data);
+
+    f << bits(a) << bits(b) << bits(c) << bits(d) << bits(e);
+}
 </pre>
 
+loading:
+
+<pre>
+static void save (std::ifstream f)
+{
+    int a, b, c;
+    std::string d;
+    std::vector<std::string> e;
+
+    f >> bits(a) >> bits(b) >> bits(c) >> bits(d) >> bits(e);
+}
+</pre>
+
+To build:
+
+<pre>
+sh ./RUNME
+c++ -std=c++2a -Werror -g -ggdb3 -O2 -Wall -fmessage-length=0 -fdiagnostics-print-source-range-info -fdiagnostics-show-category=id -fdiagnostics-parseable-fixits -Wno-trigraphs -Wmissing-prototypes -Wreturn-type -Wparentheses -Wswitch -Wno-unused-parameter -Wunused-variable -Wunused-value -Wno-sign-conversion -c -o .o/main.o main.cpp
+c++ .o/main.o   -o c_plus_plus_serializer
+</pre>
+
+To test:
+
+<pre>
+./c_plus_plus_serializer
+a 5
+b -1
+c 123456
+d hello
+e 2 vec-elems: [vec-elem1] [vec-elem2]
+f 2 list-elems: [list-elem1] [list-elem2]
+</pre>
+
+Recompile with DEBUG_C_PLUS_PLUS_SERIALIZER to debug.
--- main.cpp
+++ main.cpp
@@ -1,10 +1,60 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <assert.h>
-//#include "c_plus_plus_serializer.h"
+#include <list>
+#include <vector>
+
+#define DEBUG_C_PLUS_PLUS_SERIALIZER
+#include "c_plus_plus_serializer.h"
+
+static void save (const std::string filename)
+{
+    std::cout << "save to " << filename << std::endl;
+    std::ofstream out(filename, std::ios::binary );
+
+    int a = 5, b = -1, c = 123456;
+
+    std::string d("hello");
+
+    std::initializer_list<std::string> d1 = {"vec-elem1", "vec-elem2"};
+    std::vector<std::string> e(d1);
+
+    std::initializer_list<std::string> d2 = {"list-elem1", "list-elem2"};
+    std::list<std::string> f(d2);
+
+    out << bits(a) << bits(b) << bits(c) << bits(d) << bits(e) << bits(f);
+}
+
+static void load (const std::string filename)
+{
+    std::cout << "read from " << filename << std::endl;
+    std::ifstream in(filename);
+
+    int a, b, c;
+    std::string d;
+    std::vector<std::string> e;
+    std::list<std::string> f;
+
+    in >> bits(a) >> bits(b) >> bits(c) >> bits(d) >> bits(e) >> bits(f);
+
+    std::cout << "a " << a << std::endl;
+    std::cout << "b " << b << std::endl;
+    std::cout << "c " << c << std::endl;
+    std::cout << "d " << d << std::endl;
+    std::cout << "e " << e.size() << " vec-elems: ";
+
+    for (auto i : e) {
+        std::cout << "[" << i << "] ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "f " << f.size() << " list-elems: ";
+    for (auto i : f) {
+        std::cout << "[" << i << "] ";
+    }
+    std::cout << std::endl;
+}
 
 int main (int argc, char *argv[])
 {
+    save(std::string("data.bin"));
+    load(std::string("data.bin"));
     return (0);
 }
